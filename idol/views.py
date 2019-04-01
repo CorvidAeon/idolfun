375cfe4ce7a5e5f5ffedf9c730f45efcfab568b0
CorvidAeon <corvidaeon@gmail.com>
2019-04-01 02:34:39 -0700

    Trying out css based collapsing

--- idol/views.py
+++ idol/views.py
@@ -5,9 +5,10 @@ from .models import CardData
 
 def index(request):
     dance_idols = CardData.objects.order_by('-dance_max')[:5]
-    rarity = ['', 'N', 'N+', 'R', 'R+', 'SR', 'SR+', 'SSR', 'SSR+']
+    rarity = ['B', 'N', 'N+', 'R', 'R+', 'SR', 'SR+', 'SSR', 'SSR+']
     template = loader.get_template('card.html')
     context = {
         'dance_idols': dance_idols,
+        'rarity': rarity,
     }
     return HttpResponse(template.render(context, request))
