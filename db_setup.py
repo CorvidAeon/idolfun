81683045ac1eac77a94c47e282b0e95d670a2f1f
CorvidAeon <corvidaeon@gmail.com>
2019-04-05 02:02:03 -0700

    Starting on setup utility

---
+++ db_setup.py
@@ -0,0 +1,17 @@
+import json
+import requests
+import sqlite3
+
+api_url_base = "https://starlight.kirara.ca/api/v1/"
+
+def get_name
+
+# should only run once
+def get_all_images():
+
+# only run if object isn't already translated
+def get_name_translation(name):
+
+def get_skill_translation(skill):
+
+def get_card_translation(card):
